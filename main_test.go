89cb7cfba4680d96c6a9317643fb8a11b0b8a0ed
Numan <numan.b@arise.tech>
2024-06-30 22:46:59 +0700

    case IV  should return 4

--- main_test.go
+++ main_test.go
@@ -123,6 +123,7 @@ func Test_romantonumber(t *testing.T) {
 		{name: "case 1", args: "I", want: 1},
 		{name: "case 2", args: "II", want: 2},
 		{name: "case 3", args: "III", want: 3},
+		{name: "case 4", args: "IV", want: 4},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
