188e2897ff1578bd95b5bf9c85e074112d4c8d7f
Numan <numan.b@arise.tech>
2024-06-30 22:51:40 +0700

    case XIII  should return 13

--- main_test.go
+++ main_test.go
@@ -132,6 +132,7 @@ func Test_romantonumber(t *testing.T) {
 		{name: "case 10", args: "X", want: 10},
 		{name: "case 11", args: "XI", want: 11},
 		{name: "case 12", args: "XII", want: 12},
+		{name: "case 13", args: "XIII", want: 13},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
